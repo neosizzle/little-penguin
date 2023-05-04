e6ce5714aad2c954e563c9110baa02683d7068a2
jng <junhanngd1115@gmail.com>
2023-05-04 10:13:41 +0800

    feat: ex05 boiler

--- ex05/test.sh
+++ ex05/test.sh
@@ -1,12 +1,12 @@
 make
 insmod fortytwo.ko
-echo "bad write"
+echo "bad write echo -n "jng123" > /dev/fortytwo"
 echo -n "jng123" > /dev/fortytwo
-echo "bad write2"
+echo "bad write2 echo -n "jn" > /dev/fortytwo"
 echo -n "jn" > /dev/fortytwo
-echo "bad write3"
+echo "bad write3 echo -n "dabb" > /dev/fortytwo"
 echo -n "dabb" > /dev/fortytwo
-echo "read"
+echo "read cat /dev/fortytwo"
 cat /dev/fortytwo
 echo ""
 rmmod fortytwo.ko
