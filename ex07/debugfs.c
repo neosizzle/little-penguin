a0d3cc69f96f31b4d9da6a9a409cf2a297d372e7
jng <junhanngd1115@gmail.com>
2023-05-04 23:09:45 +0800

    feat: ex07 boiler

--- ex07/debugfs.c
+++ ex07/debugfs.c
@@ -60,7 +60,7 @@ static ssize_t foo_write(struct file *filep, const char *buffer, size_t len, lof
 	// *offset += len_to_cpy;
 	// printk(KERN_INFO "foo: write return %d at len %d\n", len_to_cpy, len);
 	// return len_to_cpy;
-	printk(KERN_INFO "foo: write return %d at len %d\n", len_to_cpy, len);
+	printk(KERN_INFO "foo: write offset %d at len %d\n", *offset, len);
 	return len;
 }
 
