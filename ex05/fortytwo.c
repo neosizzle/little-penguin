fa3a534a1f3dff4a89727cdd19f9058661b3088a
jng <junhanngd1115@gmail.com>
2023-05-04 10:07:11 +0800

    feat: ex05 boiler

--- ex05/fortytwo.c
+++ ex05/fortytwo.c
@@ -56,7 +56,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer,
 		printk(KERN_INFO "Copy from user error\n");
 		return -EFAULT;
 	}
-	if (ft_strncmp(login, message, len) != 0)
+	if (ft_strncmp(login, message, len > strlen(login) ? len : strlen(login)) != 0)
 	{
 		printk(KERN_INFO "invalid argument\n");
 		return -EINVAL;
