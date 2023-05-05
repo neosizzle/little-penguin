3a94f0b31bb93667ed7ca981e1694fe4ceccb112
jng <junhanngd1115@gmail.com>
2023-05-05 09:56:54 +0800

    feat: testcases enchance

--- ex05/test.sh
+++ ex05/test.sh
@@ -1,12 +1,22 @@
 make
 insmod fortytwo.ko
-echo "bad write echo -n "jng123" > /dev/fortytwo"
-echo -n "jng123" > /dev/fortytwo
-echo "bad write2 echo -n "jn" > /dev/fortytwo"
-echo -n "jn" > /dev/fortytwo
-echo "bad write3 echo -n "dabb" > /dev/fortytwo"
-echo -n "dabb" > /dev/fortytwo
-echo "read cat /dev/fortytwo"
-cat /dev/fortytwo
-echo ""
+# echo "bad write echo -n "jng123" > /dev/fortytwo"
+# echo -n "jng123" > /dev/fortytwo
+# echo "bad write2 echo -n "jn" > /dev/fortytwo"
+# echo -n "jn" > /dev/fortytwo
+# echo "bad write3 echo -n "dabb" > /dev/fortytwo"
+# echo -n "dabb" > /dev/fortytwo
+# echo "read cat /dev/fortytwo"
+# cat /dev/fortytwo
+# echo ""
+
+output_value="$(echo -n "jng123" > /dev/fortytwo)"
+echo $output_value
+# if [[ "$output_value" == *"asd"* ]]
+# 	then
+# 	echo Hey that\'s a large number.
+# 	pwd
+# fi
+
+
 rmmod fortytwo.ko
