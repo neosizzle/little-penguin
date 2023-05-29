e240d5b4e367de9b83332ba5e2ae1bd67df6ee88
jng <junhanngd1115@gmail.com>
2023-05-29 15:11:21 +0800

    fix: ex08 fix

--- ex08/reverse.c
+++ ex08/reverse.c
@@ -10,10 +10,10 @@ MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user,
-						size_t size, loff_t *offs);
+					size_t size, loff_t *offs);
 static ssize_t myfd_write(struct file *fp, const char __user *user,
-							size_t size, loff_t *offs);
-const static struct file_operations myfd_fops = {
+						size_t size, loff_t *offs);
+static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE, .read = &myfd_read, .write = &myfd_write};
 static struct miscdevice myfd_device = {
 	.minor = MISC_DYNAMIC_MINOR, .name = "reverse", .fops = &myfd_fops};
@@ -24,6 +24,7 @@ static int __init myfd_init(void)
 {
 	int retval;
 	retval = misc_register(&(*(&(myfd_device))));
+	
 	return 0;
 }
 
@@ -39,19 +40,19 @@ ssize_t myfd_read(struct file *fp,
 {
 	int t, i;
 	char *tmp2;
+
 	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_USER);
 	tmp = tmp2;
-	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
+	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++)
 		tmp[i] = str[t];
-	}
 	tmp[i] = 0x0;
 	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
 
 ssize_t myfd_write(struct file *fp,
-		const char __user *user,
-		size_t size,
-		loff_t *offs)
+			const char __user *user,
+			size_t size,
+			loff_t *offs)
 {
 	ssize_t res;
 
