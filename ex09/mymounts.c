95ea8fce4846e1129fff2deefd6039d5b320e63e
jng <junhanngd1115@gmail.com>
2023-05-29 15:29:04 +0800

    fix: ex09 fix

--- ex09/mymounts.c
+++ ex09/mymounts.c
@@ -2,6 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <linux/fs_struct.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("jng");
@@ -11,19 +13,29 @@ int my_open(struct inode *, struct file *)
 	return 0;
 }
 
-ssize_t my_read(struct file *, char __user *, size_t, loff_t *)
+ssize_t my_read(struct file *, char __user *buffer, size_t, loff_t * offset)
 {
 	struct dentry *curdentry;
-	printk("root   %s", current->fs->root.mnt->mnt_root->d_name.name);
+	char *res = kmalloc(69420, GFP_USER);
+
+	if (*offset >= 69420) return;
+	strcat(res, "root   ");
+	strcat(res,  current->fs->root.mnt->mnt_root->d_name.name);
+	strcat(res, "\n");
 	list_for_each_entry(curdentry,
 			    &current->fs->root.mnt->mnt_root->d_subdirs,
 			    d_child)
 	{
 		if (curdentry->d_flags & DCACHE_MOUNTED)
-			printk("%s    /%s", curdentry->d_name.name,
-					      curdentry->d_name.name);
+		strcat(res, curdentry->d_name.name);
+		strcat(res, "	/");
+		strcat(res,  curdentry->d_name.name);
+		strcat(res, "\n");
 	}
-	return 0;
+	strcat(res, "");
+	*offset = 69420;
+	copy_to_user(buffer, res, strlen(res));
+	return 69420;
 }
 
 struct proc_ops myops = {
