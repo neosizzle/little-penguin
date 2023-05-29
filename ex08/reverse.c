3d61b05c35c8fdef3cd1eb5ceeb7b60fdcb4c65d
jng <junhanngd1115@gmail.com>
2023-05-29 15:12:28 +0800

    fix: ex08 fix

--- ex08/reverse.c
+++ ex08/reverse.c
@@ -10,9 +10,9 @@ MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user,
-					size_t size, loff_t *offs);
+			size_t size, loff_t *offs);
 static ssize_t myfd_write(struct file *fp, const char __user *user,
-						size_t size, loff_t *offs);
+				size_t size, loff_t *offs);
 static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE, .read = &myfd_read, .write = &myfd_write};
 static struct miscdevice myfd_device = {
@@ -24,7 +24,7 @@ static int __init myfd_init(void)
 {
 	int retval;
 	retval = misc_register(&(*(&(myfd_device))));
-	
+
 	return 0;
 }
 
@@ -50,9 +50,9 @@ ssize_t myfd_read(struct file *fp,
 }
 
 ssize_t myfd_write(struct file *fp,
-			const char __user *user,
-			size_t size,
-			loff_t *offs)
+				const char __user *user,
+				size_t size,
+				loff_t *offs)
 {
 	ssize_t res;
 
