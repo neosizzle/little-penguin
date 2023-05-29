923c2d445c048d3d4ff2356092a776397ffb645a
jng <junhanngd1115@gmail.com>
2023-05-29 15:10:05 +0800

    fix: ex08 fix

--- ex08/reverse.c
+++ ex08/reverse.c
@@ -1,33 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-MODULE_LICENSE("LICENSE");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
 static ssize_t myfd_read(struct file *fp, char __user *user,
-			size_t size, loff_t *offs);
+						size_t size, loff_t *offs);
 static ssize_t myfd_write(struct file *fp, const char __user *user,
-			size_t size, loff_t *offs);
-static struct file_operations myfd_fops = {
+							size_t size, loff_t *offs);
+const static struct file_operations myfd_fops = {
 	.owner = THIS_MODULE, .read = &myfd_read, .write = &myfd_write};
 static struct miscdevice myfd_device = {
 	.minor = MISC_DYNAMIC_MINOR, .name = "reverse", .fops = &myfd_fops};
 char str[PAGE_SIZE];
 char *tmp;
+
 static int __init myfd_init(void)
 {
 	int retval;
 	retval = misc_register(&(*(&(myfd_device))));
 	return 0;
 }
+
 static void __exit myfd_cleanup(void)
 {
 	misc_deregister(&myfd_device);
 }
+
 ssize_t myfd_read(struct file *fp,
 				  char __user *user,
 				  size_t size,
@@ -43,6 +47,7 @@ ssize_t myfd_read(struct file *fp,
 	tmp[i] = 0x0;
 	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
+
 ssize_t myfd_write(struct file *fp,
 		const char __user *user,
 		size_t size,
@@ -51,13 +56,12 @@ ssize_t myfd_write(struct file *fp,
 	ssize_t res;
 
 	for (int i = 0; i < (PAGE_SIZE); ++i)
-	{
 		str[i] = 0;
-	}
 	res = 0;
 	res = simple_write_to_buffer(str, size, offs, user, size);
 	str[size + 1] = 0x0;
 	return res;
 }
+
 module_init(myfd_init);
 module_exit(myfd_cleanup);
